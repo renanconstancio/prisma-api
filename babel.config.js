28bc6dfa7aa5a21a1b369c1c33d23ebf9fb4ac29
Renan Constancio <renan@dcisuporte.com.br>
2022-08-10 14:12:47 -0300

    ajuste

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,26 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
     '@babel/preset-typescript'
   ],
   plugins: [
     ['module-resolver', {
       alias: {
-        "@modules": "./src/modules",
-        "@config": "./src/config",
-        "@shared": "./src/shared"
+        '@config': './src/config',
+        '@models': './src/models',
+        '@controllers': './src/controllers',
+        '@views': './src/views'
       }
-    }],
-    "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    }]
   ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
 }
